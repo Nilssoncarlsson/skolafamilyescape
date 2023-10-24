e443617681c282f05f6fdba219edc810338d9aba
csal1 <csal@live.se>
2023-10-24 22:49:46 +0200

    Update EventCard.jsx

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -4,25 +4,25 @@ import { Card, Container, Row, Col } from 'react-bootstrap';
 
 const EventTrip = [
     {
-        image: './Images/bootcamp.png',
+        image: 'https://www.wedotravel.se/upload/images/sutr/foretag/nya_mindre/marbella_golf_privatjet/eventresor_hotelclaudemarbell_11_terrass.jpg',
         heading: "Bootcamp, Marbella",
         description: "Upptäck kroppens underverk och äventyret i Marbella med hela familjen",
         price: "Res en vecka fr. 15.999",
     },
     {
-        image: './Images/safari.png',
+        image: 'https://www.firstclasstravel.se/upload/images/0-fct/huvubilder-450x800/botswana/gepard/gepard.jpg',
         heading: "Safari, Sydafrika",
         description: "Ge er ut på en spännande safari i Sydafrika och upptäck de storslagna vilda djuren ",
         price: "Res 1 veack fr. 15.995",
     },
     {
-        image: './Images/rom.png',
+        image: 'https://www.wedotravel.se/upload/images/sutr/golfresor/irland/carton_house/golf_cartonhouse_fasad.jpg',
         heading: "Historisk resa, Rom",
         description: "Res tillbaka i tiden till antika Rom och upplev historiens vingslag",
         price: "Res en vecka fr. 7.996",
     },
     {
-        image: './Images/costarica.png',
+        image: 'https://cdn.vagabond.se/app/uploads/2022/03/1054560-1536x1024.jpg',
         heading: "Äventyrsresa, Costa Rica",
         description: "Upptäck djungelns underverk och äventyret i Costa Rica",
         price: "Res 1 vecka fr. 12.995",
