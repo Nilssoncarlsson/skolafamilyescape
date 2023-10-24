b7e3e410dd6743e8dab9ddae20c17a694523effd
csal1 <csal@live.se>
2023-10-24 22:38:25 +0200

    Update SearchButton.stories.js

--- src/components/SearchButton/SearchButton.stories.js
+++ src/components/SearchButton/SearchButton.stories.js
@@ -11,13 +11,4 @@ export default {
   },
 };
 
-export const Default = {
-  args: {
-    property1: "variant-2",
-    className: {},
-    divClassName: {},
-    vectorClassName: {},
-    vector:
-      "https://cdn.animaapp.com/projects/653017b8aa739b8ec7227123/releases/65301872f8aeb1bee1f92f1f/img/vector-5.svg",
-  },
-};
+
