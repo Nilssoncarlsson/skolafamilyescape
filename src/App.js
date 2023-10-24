86908bdc35314c5570fbcb8c5afe2b2746d2ba6a
csal1 <csal@live.se>
2023-10-24 21:47:40 +0200

    zx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import {DestinationCard} from './components/DestinationCard';
 import './App.css';
 
-import EventDetails from './components/EventDetails';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import BookingForm from './components/BookingForm';
 import {PaymentContainer} from './components/PaymentSpec';
 import './paymentspec.css';
 import DebitDetails from './components/Payment/DebitDetails';
@@ -13,8 +11,7 @@ import { Footer } from './components/Footer/Footer copy/Footer';
 import HotelRooms from './components/hotelroomcard/HotelRoomCard';
 import HotelSpec from './components/HotelSpec/HotelSpec';
 import Book from './components/Booking/Book';
-
-
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
 
@@ -35,7 +32,7 @@ function App() {
      <PaymentContainer/>
      <Footer/>
      
-
+<Sidebar/>
 
 </div>
   );
